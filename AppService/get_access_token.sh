58c2c9bc795e2bb435b787ddbbb633cfee23e74e
AzSec <azsecblog@gmail.com>
2021-12-30 07:14:27 +0000

    Updated to test uri

--- AppService/get_access_token.sh
+++ AppService/get_access_token.sh
@@ -40,6 +40,7 @@ header_=$(echo ${header} | tr -d '"')
 # The managed Docker container to provide remote SSH on your web app Alpine Linux v3.13
 # The following wget is used to print the response which contains Access Token.
 # You need to re-format the access token as the output in the terminal prints access token in multiple lines
+echo ${uri}
 wget -qO- --header ${header_} ${uri}
 
 echo -e  "\e[32m[+] Copy the access token and use it separately on your workstation\e[0m"
