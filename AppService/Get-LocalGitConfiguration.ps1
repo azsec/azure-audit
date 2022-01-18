49b6c1435fe2878cd34b5a723b1f3206c4a2e21b
ri-we <rinus_w@hotmail.com>
2022-01-18 21:56:44 +0100

    Update Get-LocalGitConfiguration.ps1
    Replaced the subscription enumeration with an ARG query Search-AzGraph, the ARG query selects only the Linux runtime enabled webapps and doesn't enumerate all webapps.

--- AppService/Get-LocalGitConfiguration.ps1
+++ AppService/Get-LocalGitConfiguration.ps1
@@ -8,30 +8,31 @@
     This script is written with AzureRM PowerShell module that shall be deprecated soon. You are advised to upgrade to Azure PowerShell Az module.
 
     File Name     : Get-LocalGitConfiguration.ps1
-    Version       : 1.0.0.0
+    Version       : 1.0.0.1
     Author        : AzSec (https://azsec.azurewebsites.net/)
-  
+
   .EXAMPLE
-    .\Get-LocalGitConfiguration.ps1 -FileName webappaudit -Path C:\Workspace 
+    .\Get-LocalGitConfiguration.ps1 -FileName webappaudit -Path C:\Workspace
 #>
 
 Param(
-    [Parameter(Mandatory = $true,
-              HelpMessage = "File name of the audit report",
-              Position = 0)]
-    [ValidateNotNullOrEmpty()]
-    [string]
-    $FileName,
+  [Parameter(Mandatory = $true,
+    HelpMessage = "File name of the audit report",
+    Position = 0)]
+  [ValidateNotNullOrEmpty()]
+  [string]
+  $FileName,
 
-    [Parameter(Mandatory = $true,
-              HelpMessage = "Location where the audit report is stored",
-              Position = 1)]
-    [ValidateNotNullOrEmpty()]
-    [string]
-    $Path
+  [Parameter(Mandatory = $true,
+    HelpMessage = "Location where the audit report is stored",
+    Position = 1)]
+  [ValidateNotNullOrEmpty()]
+  [string]
+  $Path
 )
 
-$subscriptions = Get-AzSubscription
+$webapps = Search-AzGraph -Query "resources | join kind=leftouter (ResourceContainers | where type=='microsoft.resources/subscriptions' | project SubName=name, subscriptionId) on subscriptionId | where type == 'microsoft.web/sites' | where kind has 'linux' or kind has 'functionapp' | order by subscriptionId"
+
 $date = Get-Date -UFormat "%Y_%m_%d_%H%M%S"
 
 class webAppCsv {
@@ -39,61 +40,39 @@ class webAppCsv {
   [Object]${SubscriptionName}
   [Object]${WebAppName}
   [Object]${ResourceGroupName}
-  [Object]${Runtime}
   [Object]${ScmType}
 }
 
 $webAppCsvReport = @()
 
-foreach ($subscription in $subscriptions) {
-  Write-Host -ForegroundColor Green "[-] Set context for subscription:" $subscription.name
-  $context = Set-AzContext -Subscription $subscription.id
-  if ($context) {
-    Write-Host -ForegroundColor Green "[-] Start checking Web app in subscription:" $subscription.name
-    $webApps = Get-AzWebApp
-    foreach ($webApp in $webApps) {
-      $config = Get-AzResource -ResourceGroupName $webApp.ResourceGroup `
-                               -ResourceType "Microsoft.Web/sites/config" `
-                               -ResourceName "$($webApp.Name)/web" `
-                               -apiVersion 2021-02-01
-      $webAppObj = [webAppCsv]::new()
-      $webAppObj.SubscriptionId = $subscription.Id
-      $webAppObj.SubscriptionName = $subscription.Name
-      $webAppObj.WebAppName = $webApp.Name
-      $webAppObj.ResourceGroupName = $webApp.ResourceGroup
 
-      if ($($config.Properties.phpVersion)) {
-        $webAppObj.Runtime = "php" + $config.Properties.phpVersion
-      }
-      elseif ($($config.Properties.pythonVersion)) {
-        $webAppObj.Runtime = "python" + $config.Properties.pythonVersion
-      }
-      elseif ($($config.Properties.nodeVersion)) {
-        $webAppObj.Runtime = "node" + $config.Properties.nodeVersion
-      }
-      elseif ($($config.Properties.javaVersion)) {
-        $webAppObj.Runtime = "java" + $config.Properties.javaVersion
-      }
-      elseif ($($config.Properties.linuxFxVersion)) {
-        $webAppObj.Runtime = $config.Properties.linuxFxVersion
-      }
-      elseif ($($config.Properties.windowsFxVersion)) {
-        $webAppObj.Runtime = $config.Properties.windowsFxVersion
-      }
+$context = $null
+foreach ($webApp in $webApps) {
 
-      $webAppObj.ScmType = $config.Properties.ScmType
-      if ($($config.Properties.ScmType) -eq "LocalGit"){
-        Write-Host -ForegroundColor Yellow "[!] Found a vulnerable web app:"
-        Write-Host -ForegroundColor Red "`t[!] Subscription Name:" $subscription.Name
-        Write-Host -ForegroundColor Red "`t[!] Resource Group Name:" $webApp.ResourceGroup
-        Write-Host -ForegroundColor Red "`t[!] Web App Name:" $webApp.Name
-      }
-      $webAppCsvReport += $webAppObj
-    }
+  if ($context -ne $webapp.subscriptionId) {
+    Write-Host -ForegroundColor Green "[-] Set context for subscription:" $webapp.SubName
+    $context = (Set-AzContext -Subscription $webapp.subscriptionId).Subscription.Id
+    Write-Host -ForegroundColor Green "[-] Start checking Web app in subscription:" $webapp.SubName
   }
-  else {
-    throw "[!] Subscription id $($subscription.id) could not be found. Please try again!"
+
+  $config = Get-AzResource -ResourceGroupName $webapp.properties.resourceGroup `
+    -ResourceType "Microsoft.Web/sites/config" `
+    -ResourceName $webapp.properties.name `
+    -ApiVersion 2021-02-01
+  $webAppObj = [webAppCsv]::new()
+  $webAppObj.SubscriptionId = $webapp.subscriptionId
+  $webAppObj.SubscriptionName = $webapp.SubName
+  $webAppObj.WebAppName = $webapp.properties.name
+  $webAppObj.ResourceGroupName = $webapp.properties.resourceGroup
+
+  $webAppObj.ScmType = $config.Properties.ScmType
+  if ($($config.Properties.ScmType) -eq "LocalGit") {
+    Write-Host -ForegroundColor Yellow "[!] Found a vulnerable web app:"
+    Write-Host -ForegroundColor Red "`t[!] Subscription Name:" $subscription.SubName
+    Write-Host -ForegroundColor Red "`t[!] Resource Group Name:" $webapp.properties.resourceGroup
+    Write-Host -ForegroundColor Red "`t[!] Web App Name:" $webapp.properties.name
   }
+  $webAppCsvReport += $webAppObj
 }
 
 $webAppCsvReport | Export-Csv -Path "$Path\$($FileName)_$($date).csv" -NoTypeInformation -Encoding UTF8
